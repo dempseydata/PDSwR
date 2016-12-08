721f36b139bab7e389138aaf79000d4bb3fb62c7
Andrew Dempsey <dempseydata@users.noreply.github.com>
2016-12-08 11:12:38 -0800

    Add files via upload

--- Empty.R
+++ Empty.R
@@ -22,9 +22,10 @@ table(d$Purpose, d$Good.Loan)
 # once the data has been pumped into H@ following the readme instrcutions, query it
 options( java.parameters = "-Xmx2g" )
 library(RJDBC)
-drv = JDBC("org.h2.Driver","h2-1.3.170.jar",identifier.quote="'")
+drv = JDBC("org.h2.Driver","ignore/h2-1.3.170.jar",identifier.quote="'")
 options=";LOG=0;CACHE_SIZE=65536;LOCK_MODE=0;UNDO_LOG=0"
-conn = dbConnect(drv,paste("jdbc:h2:H2DB",options,sep=''),"u","u")
+# after the "h2", is where the path to the DB file is specified.
+conn = dbConnect(drv,paste("jdbc:h2:ignore/H2DB",options,sep=''),"u","u")
 dhus = dbGetQuery(conn,"SELECT * FROM hus WHERE ORIGRANDGROUP<=1")
 dpus = dbGetQuery(conn,"SELECT pus.* FROM pus WHERE pus.SERIALNO IN (SELECT DISTINCT hus.SERIALNO FROM hus WHERE hus.ORIGRANDGROUP<=1)")
 dbDisconnect(conn)
@@ -77,3 +78,90 @@ custdata <- read.table('Chpt3/custdata.tsv',header=T,sep='\t')
 summary(custdata$income)
 
 #section 3.2.1
+
+install.packages("ggplot2")
+
+library(ggplot2)
+ggplot(custdata) +
+  geom_histogram(aes(x=age),
+                 binwidth=5, fill="grey")
+
+#install.packages("scales") # 0.4.1 is already in the R
+library(scales) # brings in dollar x axis notation
+
+ggplot(custdata) +
+  geom_density(aes(x=income)) +
+  scale_x_continuous(labels=dollar)
+
+
+ggplot(custdata) +
+  geom_density(aes(x=income)) +
+  scale_x_log10(breaks=c(100,100,10000,100000), labels=dollar) +
+  annotation_logticks(sides="bt")
+
+# low cardinality categorical
+ggplot(custdata) + geom_bar(aes(x=marital.stat), fill="gray")
+
+# higher cadinality categorical
+ggplot(custdata) + geom_bar(aes(x=state.of.res), fill="gray") +
+  coord_flip() +
+  theme(axis.text.y=element_text(size=rel(0.8)))
+
+# reordering the fctors of a column, by descending size
+statesums <- table(custdata$state.of.res) #table with standard order
+statef <- as.data.frame(statesums) #stick in a data frame - could have done this with one line
+colnames(statef) <- c("state.of.res", "count") # rename the columns in the new data frame
+summary(statef) # default ordering is still alpha
+
+statef <- transform(statef, state.of.res = reorder(state.of.res,count))
+# order is now by count ascending
+
+ggplot(statef) + geom_bar(aes(x=state.of.res,y=count), stat="identity", fill="gray") +
+  coord_flip() +
+  theme(axis.text.y=element_text(size=rel(0.8)))
+
+
+#3.2.2
+
+custdata2 <- subset(custdata, (custdata$age > 0 & custdata$age < 100 & custdata$income > 0))
+
+cor(custdata2$age, custdata2$income)
+
+ggplot(custdata2, aes(x=age, y=income)) +
+  geom_point() +
+  stat_smooth() +
+  ylim(0, 200000)
+
+#install.packages("hexbin")
+
+library(hexbin)
+
+ggplot(custdata2, aes(x=age, y=income))+
+  geom_hex(binwidth=c(5,10000))+
+  geom_smooth(color="white", se=F)+ # se=F removes the standard error boundaries
+  ylim(0,200000)
+
+ggplot(custdata2) +
+  geom_bar(aes(x=marital.stat), position="dodge", fill="darkgray")+
+  facet_wrap(~housing.type, scales="free_y")+ #free y loosens up each y axis to be independent, else all have same scale
+  theme(axis.text.x =element_text(angle=45,hjust=1))
+
+# chpater 4
+load("Chpt4/exampleData.rData")
+summary(custdata)
+summary(hhdata)
+summary(medianincome)
+
+# looking at the columns each with 56 missing housing.type
+summary(custdata[is.na(custdata$housing.type),c("recent.move","num.vehicles")])
+# and they are the same 56 records with 3 col values of NA
+
+# replace NA in is.employed with "missing"
+
+#well, more like ADDING a new column so as to not alter the original data
+custdata$is.employed.fix <- ifelse(is.na(custdata$is.employed),"missing",
+       ifelse(custdata$is.employed==T,"employed","not employed"))
+
+summary(as.factor(custdata$is.employed.fix))
+
+# in 4.1.1, about pg140
